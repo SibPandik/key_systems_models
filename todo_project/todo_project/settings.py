010acde521ea0b8f67747c724a6052f09c204d71
Pandik <mr.kot.2018@mail.ru>
2023-07-06 11:51:14 +0000

    rename static urls

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5@o!c-$fgaihl)-#e^^*tru$ea$f4skilac-i0f7%+5q*q+66^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pandikk.pythonanywhere.com']
 
 
 # Application definition
@@ -130,9 +130,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
